ca533e8c24efcb85ef48ab5ff553b72ff7df4c6d
Edward Tran <edtran@microsoft.com>
2021-12-16 19:39:01 -0800

    Render Chat Widget using WebView via local html asset

--- app/src/main/java/com/example/embedlcw/MainActivity.kt
+++ app/src/main/java/com/example/embedlcw/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.embedlcw
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+
 
 class MainActivity : AppCompatActivity() {
+    var debug = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (debug) {
+            WebView.setWebContentsDebuggingEnabled(debug)
+        }
+
+        val wv = findViewById<WebView>(R.id.web_view)
+        wv.settings.javaScriptEnabled = true
+        wv.settings.domStorageEnabled = true
+
+//        wv.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+//        wv.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+//        wv.webChromeClient = WebChromeClient()
+
+        wv.loadUrl("file:///android_asset/index.html")
     }
 }
