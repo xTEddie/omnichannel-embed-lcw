4ca394fec4463caf918a4ad7aac794ddb0e14a1f
Edward Tran <edtran@microsoft.com>
2022-01-05 05:07:50 -0800

    Add implementation to handle file attachment downloads with images

--- app/src/main/java/com/example/embedlcw/MainActivity.kt
+++ app/src/main/java/com/example/embedlcw/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.embedlcw
 
-import android.app.Application
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.webkit.*
-import java.io.File
-import java.net.URL
+import android.webkit.DownloadListener
+import android.webkit.WebView
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,21 +21,24 @@ class MainActivity : AppCompatActivity() {
         wv.settings.javaScriptEnabled = true
         wv.settings.domStorageEnabled = true
 
-//        wv.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-//        wv.settings.cacheMode = WebSettings.LOAD_DEFAULT
-
-//        wv.webChromeClient = WebChromeClient()
-
-//        var url = URL(OmnichannelConfig.config["bootstrapper"])
-//        val baseUrl = "${url.protocol}://${url.host}"
-//
-//        val data = application.assets.open("index.html").bufferedReader().use {
-//            it.readText()
-//        };
-
-//        wv.loadDataWithBaseURL(baseUrl, data, "text/html", null, baseUrl)
-
+        // Load local .html with baseUrl set to LCW production domain since attachment downloads does not work cross-origin
         val queryParams = "orgId=${OmnichannelConfig.config["orgId"]}&orgUrl=${OmnichannelConfig.config["orgUrl"]}&appId=${OmnichannelConfig.config["appId"]}&hideChatbutton=${OmnichannelConfig.config["hideChatbutton"]}&renderMobile=${OmnichannelConfig.config["renderMobile"]}&src=${OmnichannelConfig.config["src"]}"
-        wv.loadUrl("${OmnichannelConfig.config["webPage"]}?${queryParams}")
+        val baseUrl = "https://oc-cdn-ocprod.azureedge.net?${queryParams}"
+        val data = application.assets.open("index.html").bufferedReader().use {
+            it.readText()
+        };
+
+        wv.loadDataWithBaseURL(baseUrl, data, "text/html", null, baseUrl)
+
+        // Expose Android methods to Javascript layer
+        val javascriptInterface = JavascriptInterface(applicationContext)
+        wv.addJavascriptInterface(javascriptInterface, "Android")
+
+        // Subscribe to notification when a file from Web content needs to be downloaded in Android layer
+        wv.setDownloadListener(DownloadListener { url, _, _, _, _ ->
+            if (url.startsWith("blob:")) {
+                wv.evaluateJavascript(javascriptInterface.getBase64StringFromBlobUrl(url), null)
+            }
+        })
     }
 }
