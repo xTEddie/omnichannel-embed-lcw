f685c6924b9ec1eee4f64e9722cfc59f2d916ed4
Edward Tran <edtran@microsoft.com>
2022-01-26 16:19:14 -0800

    Add option to preload assets on init instead of at runtime

--- android-embed-lcw/app/src/main/java/com/example/embedlcw/LocalAssetsWebViewClient.kt
+++ android-embed-lcw/app/src/main/java/com/example/embedlcw/LocalAssetsWebViewClient.kt
@@ -1,13 +1,37 @@
 package com.example.embedlcw
 
+import android.opengl.Visibility
 import android.util.Log
+import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import java.io.InputStream
 
-class LocalAssetsWebViewClient() : WebViewClient() {
+class LocalAssetsWebViewClient(private var wv: WebView) : WebViewClient() {
+    private var preload = false; // Use preloaded assets on init instead of at runtime
     private var useLocalAssets = false;
+    private lateinit var localAssets: Map<String, InputStream>;
+
+    init {
+        Log.i("LocalAssetsWebViewClient", "Init")
+        val application = wv.context.applicationContext
+        if (preload) {
+            localAssets = mapOf(
+                "webchat-es5.js" to application.assets.open("botframework-webchat/4.9.2/webchat-es5.js"),
+                "chat-adapter-0.0.35-beta.1.js" to application.assets.open("chat-adapter-0.0.35-beta.1.js"),
+                "webchat-ic3adapter/0.1.0-master.2dba07b/botframework-webchat-adapter-ic3.production.min.js" to application.assets.open(
+                    "webchat-ic3adapter/0.1.0-master.2dba07b/botframework-webchat-adapter-ic3.production.min.js"
+                ),
+                "jquery-3.4.1.min.js" to application.assets.open("jquery-3.4.1.min.js"),
+                "ocsdk/0.2.1-main.d3114ea/SDK.min.js" to application.assets.open("ocsdk/0.2.1-main.d3114ea/SDK.min.js"),
+                "purify.min.js" to application.assets.open("purify.min.js"),
+                "ams/0.1.0-main.ef0152f/SDK.min.js" to application.assets.open("ams/0.1.0-main.ef0152f/SDK.min.js"),
+                "appinsights.js" to application.assets.open("appinsights.js")
+            )
+        }
+    }
 
     /**
      * Intercepts resources and load them from local assets.
@@ -29,7 +53,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("botframework-webchat/4.9.2/webchat-es5.js")
+                        if (preload) localAssets["webchat-es5.js"] else application.assets.open("botframework-webchat/4.9.2/webchat-es5.js")
                     )
                 }
 
@@ -37,7 +61,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("chat-adapter-0.0.35-beta.1.js")
+                        if (preload) localAssets["chat-adapter-0.0.35-beta.1.js"] else application.assets.open("chat-adapter-0.0.35-beta.1.js")
                     )
                 }
 
@@ -47,7 +71,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("webchat-ic3adapter/0.1.0-master.2dba07b/botframework-webchat-adapter-ic3.production.min.js")
+                        if (preload) localAssets["webchat-ic3adapter/0.1.0-master.2dba07b/botframework-webchat-adapter-ic3.production.min.js"] else application.assets.open("webchat-ic3adapter/0.1.0-master.2dba07b/botframework-webchat-adapter-ic3.production.min.js")
                     )
                 }
 
@@ -55,7 +79,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("jquery-3.4.1.min.js")
+                        if (preload) localAssets["jquery-3.4.1.min.js"] else application.assets.open("jquery-3.4.1.min.js")
                     )
                 }
 
@@ -66,7 +90,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("ocsdk/0.2.1-main.d3114ea/SDK.min.js")
+                        if (preload) localAssets["ocsdk/0.2.1-main.d3114ea/SDK.min.js"] else application.assets.open("ocsdk/0.2.1-main.d3114ea/SDK.min.js")
                     )
                 }
 
@@ -74,7 +98,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("purify.min.js")
+                        if (preload) localAssets["purify.min.js"] else application.assets.open("purify.min.js")
                     )
                 }
 
@@ -85,7 +109,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("ams/0.1.0-main.ef0152f/SDK.min.js")
+                        if (preload) localAssets["ams/0.1.0-main.ef0152f/SDK.min.js"] else application.assets.open("ams/0.1.0-main.ef0152f/SDK.min.js")
                     )
                 }
 
@@ -93,7 +117,7 @@ class LocalAssetsWebViewClient() : WebViewClient() {
                     return WebResourceResponse(
                         "text/javascript",
                         "gzip",
-                        application.assets.open("appinsights.js")
+                        if (preload) localAssets["appinsights.js"] else application.assets.open("appinsights.js")
                     )
                 }
             } catch (e: Exception) {
